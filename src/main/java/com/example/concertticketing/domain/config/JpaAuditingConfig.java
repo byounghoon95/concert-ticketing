14eaf7bd107d7ec0739ab2daa709ea10ddba65b8
owner <toltolyibh@gmail.com>
2024-07-14 21:24:58 +0900

    스웨거 및 Auditing 설정파일 추가

---
+++ src/main/java/com/example/concertticketing/domain/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.example.concertticketing.domain.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
