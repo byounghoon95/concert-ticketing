ca877aff9920da244dd713b3cf09153fea4475fb
owner <toltolyibh@gmail.com>
2024-07-14 21:50:23 +0900

    토큰 검증 인터셉터 추가

---
+++ src/main/java/com/example/concertticketing/domain/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.example.concertticketing.domain.config;
+
+import com.example.concertticketing.domain.common.interceptor.TokenVerificationInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final TokenVerificationInterceptor tokenVerificationInterceptor;
+
+    @Autowired
+    public WebConfig(TokenVerificationInterceptor tokenVerificationInterceptor) {
+        this.tokenVerificationInterceptor = tokenVerificationInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(tokenVerificationInterceptor)
+                .addPathPatterns("/api/concert/**", "/api/reserve/**", "/api/pay/**");
+    }
+}
