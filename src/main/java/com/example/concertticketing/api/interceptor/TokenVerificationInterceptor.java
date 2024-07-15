7a98b3b3cd1a051d31c7acf380f0749d501da488
owner <toltolyibh@gmail.com>
2024-07-15 13:21:04 +0900

    인터셉터 위치 domain -> presentation 으로 변경

--- src/main/java/com/example/concertticketing/api/interceptor/TokenVerificationInterceptor.java
+++ src/main/java/com/example/concertticketing/api/interceptor/TokenVerificationInterceptor.java
@@ -1,14 +1,16 @@
-package com.example.concertticketing.domain.common.interceptor;
+package com.example.concertticketing.api.interceptor;
 
 import com.example.concertticketing.domain.exception.CustomException;
 import com.example.concertticketing.domain.exception.ErrorEnum;
 import com.example.concertticketing.domain.queue.service.QueueService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+@Slf4j
 @Component
 public class TokenVerificationInterceptor implements HandlerInterceptor {
 
@@ -22,7 +24,7 @@ public class TokenVerificationInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         Long memberId = Long.valueOf(request.getHeader("memberId"));
-
+        log.info("Member Verification Request : {}", memberId);
         if (!queueService.verify(memberId)) {
             throw new CustomException(ErrorEnum.TOKEN_EXPIRED);
         }
