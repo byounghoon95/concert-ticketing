52bdb00ddfeff02d2c6c2fc0176cf30858d5bcfd
owner <toltolyibh@gmail.com>
2024-09-19 00:51:28 +0900

    Queue 도메인 구현

---
+++ queue-service/src/main/java/com/example/queueservice/interfaces/scheduler/ExpireScheduler.java
@@ -0,0 +1,39 @@
+package com.example.queueservice.interfaces.scheduler;
+
+import com.example.queueservice.domain.service.QueueService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class ExpireScheduler {
+
+    private final QueueService queueService;
+
+    /**
+     * 활성화된 큐를 만료
+     * */
+    @Scheduled(fixedDelay = 30000) // 30초
+    public void processExpiredQueues() {
+        long start = System.currentTimeMillis();
+        queueService.expireActiveTokens(LocalDateTime.now());
+        long end = System.currentTimeMillis();
+        log.info("대기열 만료 API 소요시간 : {} ms", (end - start));
+    }
+
+    /**
+     * 대기중인 큐를 활성화
+     * */
+    @Scheduled(fixedDelay = 30000) // 30초
+    public void processActiveToken() {
+        long start = System.currentTimeMillis();
+        queueService.updateWaitTokenToActive(LocalDateTime.now(), 500);
+        long end = System.currentTimeMillis();
+        log.info("대기열 활성화 API 소요시간 : {} ms", (end - start));
+    }
+}
